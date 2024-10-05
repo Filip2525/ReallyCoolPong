40d45b7802a696897c53f7faaaeb3e7f1ef3aef7
Filip2525 <filip.stefko2525@gmail.com>
2024-10-05 19:50:19 +0200

    Fixed movement

--- src/PlayerScript.gd
+++ src/PlayerScript.gd
@@ -1,20 +1,16 @@
-extends Sprite2D
+extends CharacterBody2D
 
-var colided = false
-@export var speed = 3
+@export var speed = 15
 @export var up_key: String
 @export var down_key: String
 
 func _process(delta) -> void:
-	
-	var velocity = Vector2.ZERO
-	
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed(up_key):
 		velocity = Vector2(0,-speed)
 	if Input.is_action_pressed(down_key):
 		velocity = Vector2(0, speed)
 	
-	if not colided:
-		self.position += velocity*delta*60
+	move_and_collide(velocity*delta*speed)
 
 	
